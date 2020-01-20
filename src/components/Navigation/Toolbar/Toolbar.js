b0878f75130e1db7d0fa9007adcc65c54c4cc4d7
cesarecamurani <cesare@bitpesa.co>
2020-01-20 17:20:26 +0000

    Added SideDrawer - Reorganized Navigation files

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import classes from './Toolbar.css'
 import Logo from '../../Logo/Logo'
-import NavigationItems from './NavigationItems/Navigationitems'
+import NavigationItems from '../NavigationItems/Navigationitems'
 
 const toolbar = (props) => (
   <header className={classes.Toolbar}>
